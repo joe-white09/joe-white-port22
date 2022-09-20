fc680ba53ec3d55d77d83a2b26ceca965e91d3f0
joe-white09 <joe_white09@yahoo.com>
2022-09-20 17:41:54 -0500

    updated projects to include description, updated resume for download

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -6,6 +6,7 @@ const ProjectList = () => {
             name: 'adoptables',
             title: 'Adoptables',
             alt: 'Adoption site for pets',
+            description: 'A site that allows users to find pets in their area that have recently been put up for adoption.',
             deployedUrl: 'https://nangtuong.github.io/Adoptables/',
             gitHubUrl: 'https://github.com/NangTuong/Adoptables'
         },
@@ -13,6 +14,7 @@ const ProjectList = () => {
             name: 'living-local',
             title: 'Living Local',
             alt: 'Forum to search local activities',
+            description: 'For those looking for local insight on things to do or places to eat, check out this forum to see what people have posted in their area.',
             deployedUrl: 'https://living-locally.herokuapp.com/',
             gitHubUrl: 'https://github.com/coneboybebop/project-2-group-6'
         },
@@ -20,12 +22,14 @@ const ProjectList = () => {
             name: 'portfolio',
             title: 'My first Portfolio',
             alt: "Joe White's first portfolio project",
+            description: 'My first showcase of projects in comparison to this most recent to display how much my skill has improved in such a short time of 6 months.',
             deployedUrl: 'https://joe-white09.github.io/joe-white-portfolio/',
             gitHubUrl: 'https://github.com/joe-white09/joe-white-portfolio'
         },
         {
             name: 'team-profile-generator',
             title: 'Team Profile Generator',
+            description: 'A node application to create an html page through the command line to display a team with corresponding contact information.',
             alt: 'Node application to create a HTML page',
             deployedUrl: '',
             gitHubUrl: 'https://github.com/joe-white09/team-profile-generator'
@@ -34,6 +38,7 @@ const ProjectList = () => {
             name: 'the-tech-blog',
             title: 'The Tech Blog',
             alt: 'Forum to post and comment on latest technology',
+            description: 'Site that allows users to see new articles that others have shared, as well as log in to create posts and leave comments for other users.',
             deployedUrl: 'https://aqueous-woodland-50367.herokuapp.com/',
             gitHubUrl: 'https://github.com/joe-white09/the-tech-blog'
         },
@@ -41,6 +46,7 @@ const ProjectList = () => {
             name: 'weather-dashboard',
             title: 'Weather Dashboard',
             alt: 'Weather forecast application',
+            description: 'Application that allows users to search by city name to obtain the current weather conditions as well as the 5-day forecast.',
             deployedUrl: 'https://joe-white09.github.io/weather-dashboard/',
             gitHubUrl: 'https://github.com/joe-white09/weather-dashboard'
         }
@@ -58,6 +64,7 @@ const ProjectList = () => {
                             
                         />
                         <h3>{image.title}</h3>
+                        <p>{image.description}</p>
                         {image.deployedUrl === '' ? (
                             <p>Project is not deployed online</p>
                         ) : (
